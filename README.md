58fe0df6980afbc6e4e8e049739adacb258b86f2
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2015-01-29 22:35:17 +0200

    Add UDP mode

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-### TCP proxy in Go
+### TCP and UDP proxy in Go
 
+UDP proxying is unidirectional.
 Usage:
 
-    $ goproxy [listen-ip]:port [connect-to-ip]:port
-    Flags:
+    $ goproxy [flags] [listen-ip]:port [connect-to-ip]:port
       -debug=false: Print every connection information
       -dns="": DNS server address, supply host:port; will use system default if not set
       -dns-interval=20s: Time interval between DNS queries
       -timeout=10s: TCP connect timeout
-      -verbose=false: Print noticable info
+      -udp=false: UDP mode
+      -verbose=false: Print noticeable info
 
 Viva [go-nuts](https://groups.google.com/forum/#!topic/golang-nuts/zzW0GL4AP3k)!
--- main.go
+++ main.go
@@ -9,18 +9,16 @@ import (
 	"net"
 	"os"
 	"os/signal"
-	"sync/atomic"
 	"syscall"
 	"time"
 )
 
 var (
 	flags       = flag.NewFlagSet("goproxy", flag.ExitOnError)
+	udp         bool
 	dnsAddress  string
 	dnsInterval time.Duration
 	timeout     time.Duration
-	dnsClient   *dns.Client
-	connectTo   atomic.Value
 	verbose     bool
 	debug       bool
 )
@@ -35,6 +33,18 @@ func main() {
 		os.Exit(1)
 	}
 
+	// ignore HUP and PIPE signals
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGPIPE)
+	go func() {
+		for range c {
+		}
+	}()
+
+	// channels to pass DNS updates and new incoming connections
+	resolver := make(chan string, 1)
+	manager := make(chan net.Conn, 10)
+
 	to := flags.Arg(1)
 	if verbose {
 		log.Printf("Will connect to `%s`\n", to)
@@ -43,50 +53,62 @@ func main() {
 		if verbose {
 			log.Printf("DNS server provided: `%s`, will refresh every %v\n", dnsAddress, dnsInterval)
 		}
-		go refreshDns(to)
+		go refreshDns(to, resolver)
 	} else {
-		connectTo.Store(&to)
+		resolver <- to
 	}
 
 	on := flags.Arg(0)
 	if verbose {
-		log.Printf("Will listen on `%s`\n", on)
-	}
-	listener, err := net.Listen("tcp", on)
-	if err != nil {
-		log.Fatalf("Failed to setup TCP listener on `%s`: %v\n", on, err)
-	}
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGHUP, syscall.SIGPIPE)
-	go func() {
-		for range c {
+		proto := "tcp"
+		if udp {
+			proto = "udp"
 		}
-	}()
+		log.Printf("Will listen on `%s://%s`\n", proto, on)
+	}
 
-	for {
-		conn, err := listener.Accept()
+	if udp {
+		laddr, err := net.ResolveUDPAddr("udp", on)
 		if err != nil {
-			log.Printf("Failed to accept connection: %v", err)
-		} else {
-			go forward(conn)
+			log.Fatalf("Error resolving `%s`: %v\n", on, err)
+		}
+		conn, err := net.ListenUDP("udp", laddr)
+		if err != nil {
+			log.Fatalf("Failed to setup UDP listener on `%s`: %v\n", on, err)
+		}
+		manager <- conn
+		manageUdp(resolver, manager)
+	} else {
+		listener, err := net.Listen("tcp", on)
+		if err != nil {
+			log.Fatalf("Failed to setup TCP listener on `%s`: %v\n", on, err)
+		}
+		go manageTcp(resolver, manager)
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Printf("Failed to accept connection: %v\n", err)
+			} else {
+				manager <- conn
+			}
 		}
 	}
 }
 
 func usage() {
 	fmt.Fprintf(os.Stderr,
-		`Usage: %s [listen-ip]:port [connect-to-ip]:port
+		`Usage: %s [flags] [listen-ip]:port [connect-to-ip]:port
 Flags:
 `, os.Args[0])
 	flags.PrintDefaults()
 }
 
 func parseFlags() {
+	flags.BoolVar(&udp, "udp", false, "UDP mode")
 	flags.StringVar(&dnsAddress, "dns", "", "DNS server address, supply host:port; will use system default if not set")
 	flags.DurationVar(&dnsInterval, "dns-interval", 20*time.Second, "Time interval between DNS queries")
 	flags.DurationVar(&timeout, "timeout", 10*time.Second, "TCP connect timeout")
-	flags.BoolVar(&verbose, "verbose", false, "Print noticable info")
+	flags.BoolVar(&verbose, "verbose", false, "Print noticeable info")
 	flags.BoolVar(&debug, "debug", false, "Print every connection information")
 	flags.Usage = usage
 	flags.Parse(os.Args[1:])
@@ -95,22 +117,23 @@ func parseFlags() {
 	}
 }
 
-// https://github.com/benschw/dns-clb-go/blob/master/dns/lib.go
-func refreshDns(_connectTo string) {
-	host, port, err := net.SplitHostPort(_connectTo)
+func refreshDns(connectTo string, dnsUpdates chan string) {
+	host, port, err := net.SplitHostPort(connectTo)
 	if err != nil {
-		log.Fatalf("Error parsing `%s`: %v\n", _connectTo, err)
+		log.Fatalf("Error parsing `%s`: %v\n", connectTo, err)
 	}
 	if host == "" {
 		if verbose {
-			log.Printf("Only port is provided in `%s`, DNS server address is unused\n", _connectTo)
+			log.Printf("Only port is provided in `%s`, DNS server address is unused\n", connectTo)
 		}
-		connectTo.Store(&_connectTo)
+		dnsUpdates <- connectTo
 		return
 	}
-	dnsClient = &dns.Client{}
+	// https://github.com/benschw/dns-clb-go/blob/master/dns/lib.go
+	dnsClient := &dns.Client{}
 	name := dns.Fqdn(host)
 	qType := dns.StringToType["A"]
+	old := "(none)"
 
 	queryDns := func() {
 		req := &dns.Msg{}
@@ -142,19 +165,13 @@ func refreshDns(_connectTo string) {
 				log.Printf("DNS response has no A record: %+v\n", resp)
 			}
 		} else {
-			if verbose {
-				_old := connectTo.Load()
-				var old string
-				if _old == nil {
-					old = "(none)"
-				} else {
-					old = *_old.(*string)
-				}
-				if old != to {
+			if old != to {
+				dnsUpdates <- to
+				if verbose {
 					log.Printf("Connect target changed `%s` -> `%s`\n", old, to)
 				}
+				old = to
 			}
-			connectTo.Store(&to)
 		}
 	}
 
@@ -169,22 +186,32 @@ func refreshDns(_connectTo string) {
 	}
 }
 
-func forward(conn net.Conn) {
+func manageTcp(resolver chan string, connections chan net.Conn) {
+	var connectTo string
+	for {
+		select {
+		case connectTo = <-resolver:
+
+		case in := <-connections:
+			if connectTo != "" {
+				go forwardTcp(in, connectTo)
+			} else {
+				if debug {
+					log.Print("Don't know where to connect, closing incoming connection\n")
+				}
+				in.Close()
+			}
+		}
+	}
+}
+
+func forwardTcp(conn net.Conn, connectTo string) {
 	if debug {
 		log.Print("Accepted connection\n")
 	}
-	_to := connectTo.Load()
-	if _to == nil {
-		if debug {
-			log.Print("Don't know where to connect, closing incoming connection\n")
-		}
-		conn.Close()
-		return
-	}
-	to := *_to.(*string)
-	fwd, err := net.DialTimeout("tcp", to, timeout)
+	fwd, err := net.DialTimeout("tcp", connectTo, timeout)
 	if err != nil {
-		log.Printf("Conection to `%s` failed: %v\n", to, err)
+		log.Printf("Conection to `%s` failed: %v\n", connectTo, err)
 		conn.Close()
 		return
 	}
@@ -196,14 +223,42 @@ func forward(conn net.Conn) {
 		defer close()
 		io.Copy(fwd, conn)
 		if debug {
-			log.Print("Incoming connection closed\n")
+			log.Print("Incoming TCP connection closed\n")
 		}
 	}()
 	go func() {
 		defer close()
 		io.Copy(conn, fwd)
 		if debug {
-			log.Print("Outgoing connection closed\n")
+			log.Print("Outgoing TCP connection closed\n")
 		}
 	}()
+}
+
+func manageUdp(resolver chan string, connections chan net.Conn) {
+	var in *net.Conn
+	var out *net.Conn
+	for {
+		select {
+		case connectTo := <-resolver:
+			if out != nil {
+				(*out).Close()
+			}
+			_out, err := net.Dial("udp", connectTo)
+			if err != nil {
+				log.Printf("Conection to `%s` failed: %v\n", connectTo, err)
+			} else {
+				out = &_out
+				if in != nil {
+					go io.Copy(*out, *in)
+				}
+			}
+
+		case _in := <-connections:
+			in = &_in
+			if out != nil {
+				go io.Copy(*out, *in)
+			}
+		}
+	}
 }
